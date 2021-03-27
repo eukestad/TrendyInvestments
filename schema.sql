4f073548709d71fb25c5a8b57a451f0ec4f9f4e0
eukestad <69314608+eukestad@users.noreply.github.com>
2021-03-27 13:58:27 -0500

    adding schema and etl notebook

---
+++ schema.sql
@@ -0,0 +1,34 @@
+-- Database: investments_db
+
+-- DROP DATABASE investments_db;
+
+CREATE DATABASE investments_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+create table inx_data (
+	id serial primary key,
+	date date,
+	open decimal,
+	high decimal,
+	low decimal,
+	close decimal,
+	daily_change decimal,
+	ticker varchar(3)
+);
+
+create table var_data (
+	id serial primary key,
+	date date,
+	open decimal,
+	high decimal,
+	low decimal,
+	close decimal,
+	daily_change decimal,
+	ticker varchar(3)
+);
